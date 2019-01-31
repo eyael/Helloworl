cccec7e975d889713c38cf481705a3f0b838cd7a
eyael <eyaelja1@gmail.com>
2019-01-31 01:09:11 -0500

    Initial commit

---
+++ src/myjava.java
@@ -0,0 +1,17 @@
+/*
+ *  Comments Example Program
+ *  Dave Wolf
+ *  July 21st, 2015
+ */
+public class myjava{
+    public static void main( String[] args )
+    {
+        // This line will be ignored by the compiler
+        // but people will (hopefully) read it.
+        // Anything after the // is ignored by Java.
+        System.out.println( "This line prints just fine." ); // I can include a comment after the working code..
+        // This next line of code has been disabled by a comment:
+        // System.out.println("This line won't print.");
+        System.out.println( "This line also prints just fine." );
+    }
+}
